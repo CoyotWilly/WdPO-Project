bfd87257f756f14647aecc81802e6cb7fa7f6013
CoyotWilly <coyotwilly@gmail.com>
2022-11-30 23:09:12 +0100

    lib imports updated

--- detect.py
+++ detect.py
@@ -5,6 +5,7 @@ from typing import Dict
 import click
 import cv2
 from tqdm import tqdm
+import numpy as np
 
 
 def detect(img_path: str) -> Dict[str, int]:
@@ -20,15 +21,38 @@ def detect(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-
     #TODO: Implement detection method.
+    # (hMin = 23 , sMin = 36, vMin = 63), (hMax = 179 , sMax = 255, vMax = 255)
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     
     red = 0
     yellow = 0
     green = 0
     purple = 0
 
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lb = np.array([23, 36, 63], np.uint8)
+    ub = np.array([179, 255, 255], np.uint8)
+
+    mask = cv2.inRange(hsv, lb, ub)
+
+    blure = cv2.GaussianBlur(mask, (3, 3), 0)
+    eges = cv2.Canny(blure, 10, 100)
+    # eges = cv2.bilateralFilter(eges, 9, 75, 75)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dilatet = cv2.dilate(eges, kernel, iterations=1)
+
+    # contours
+    contours, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    img2 = img.copy()
+    cv2.drawContours(img2, contours, -1, (0, 255, 0), 2)
+    print(len(contours))
+    #
+    cv2.imshow('Image', img2)
+    cv2.imshow('Orginal', img)
+    cv2.imshow('dilitate', eges)
+    cv2.waitKey()
     return {'red': red, 'yellow': yellow, 'green': green, 'purple': purple}
 
 
@@ -49,4 +73,5 @@ def main(data_path: Path, output_file_path: Path):
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    detect('data/25.jpg')
