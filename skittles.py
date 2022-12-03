88e571c8681749dfd7e339f79dc0941103026412
CoyotWilly <coyotwilly@gmail.com>
2022-12-03 14:13:38 +0100

    yellow green and red detection added

--- skittles.py
+++ skittles.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2
 
 # VALUES FOR FUTURE IMPLEMENTATION
@@ -25,32 +24,120 @@ green = 0
 purple = 0
 
 
-img = cv2.imread('data/00.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('data/03.jpg', cv2.IMREAD_COLOR)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-blur = 0
 
-# blur-ing process
-for i in range(5):
-    blur = cv2.medianBlur(hsv, 11)
+def green_count():
+    blur = 0
 
-mask_y = cv2.inRange(blur, (35, 189, 0), (69, 255, 255))
-ker_y = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
-mask_close_y = cv2.morphologyEx(mask_y, cv2.MORPH_CLOSE, ker_y)
-mask_open_y = cv2. morphologyEx(mask_close_y, cv2.MORPH_OPEN, ker_y)
+    # blur-ing process
+    for i in range(5):
+        blur = cv2.medianBlur(hsv, 11)
 
-ker_y = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-dilatet = cv2.dilate(mask_open_y, ker_y, iterations=2)
-#
-cnt_y, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # morphology
+    mask_g = cv2.inRange(blur, (35, 189, 0), (69, 255, 255))
+    ker_g = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
+    mask_close_g = cv2.morphologyEx(mask_g, cv2.MORPH_CLOSE, ker_g)
+    mask_open_g = cv2. morphologyEx(mask_close_g, cv2.MORPH_OPEN, ker_g)
 
-for i in range(len(cnt_y)):
-    if len(cnt_y[i]) > 160:
-        yellow = yellow + 1
-print('yellow=', yellow)
-print('Counted=', len(cnt_y))
+    # object counting
+    ker_g = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilatet = cv2.dilate(mask_open_g, ker_g, iterations=1)
 
-y_mask_final = cv2.cvtColor(mask_open_y, cv2.COLOR_GRAY2BGR)
-y_final = cv2. addWeighted(y_mask_final, 0.5, img, 0.5, 0)
+    cnt_g, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# cv2.imshow('close mask', y_final)
-cv2.waitKey(0)
+    # final result display and human check
+    print('Counted=', len(cnt_g))
+    g_mask_final = cv2.cvtColor(mask_open_g, cv2.COLOR_GRAY2BGR)
+    g_final = cv2.addWeighted(g_mask_final, 0.5, img, 0.5, 0)
+
+    cv2.imshow('close mask', g_final)
+    cv2.waitKey(0)
+
+
+def yellow_count():
+    blur = 0
+    for i in range(5):
+        blur = cv2.medianBlur(hsv, 11)
+
+    # morphology
+    mask_y = cv2.inRange(blur, (0, 220, 116), (32, 255, 255))
+    ker_y = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12,12))
+    mask_close_y = cv2.morphologyEx(mask_y, cv2.MORPH_CLOSE, ker_y)
+    mask_open_y = cv2.morphologyEx(mask_close_y, cv2.MORPH_OPEN, ker_y)
+    # cv2.imshow('maks', mask_open_y)
+
+    # object counting
+    ker_y = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilatet = cv2.dilate(mask_open_y, ker_y, iterations=1)
+
+    cnt_y, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result display and human check
+    print('Counted=', len(cnt_y))
+    y_mask_final = cv2.cvtColor(mask_open_y, cv2.COLOR_GRAY2BGR)
+    y_final = cv2.addWeighted(y_mask_final, 0.5, img, 0.5, 0)
+
+    cv2.imshow('close mask', y_final)
+    cv2.waitKey(0)
+
+
+def red_count():
+    blur = 0
+    for i in range(5):
+        blur = cv2.medianBlur(hsv, 11)
+
+    # morphology
+    mask_r = cv2.inRange(blur, (170, 102, 153), (179, 255, 255))
+    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    mask_close_r = cv2.morphologyEx(mask_r, cv2.MORPH_CLOSE, ker_r)
+    mask_open_r = cv2.morphologyEx(mask_close_r, cv2.MORPH_OPEN, ker_r)
+    # cv2.imshow('maks', mask_open_r)
+
+    # object counting
+    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilatet = cv2.dilate(mask_open_r, ker_r, iterations=1)
+
+    cnt_r, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result display and human check
+    print('Counted=', len(cnt_r))
+    r_mask_final = cv2.cvtColor(mask_open_r, cv2.COLOR_GRAY2BGR)
+    r_final = cv2.addWeighted(r_mask_final, 0.5, img, 0.5, 0)
+
+    cv2.imshow('close mask', r_final)
+    cv2.waitKey(0)
+
+
+def purple_count():
+    blur = 0
+    for i in range(5):
+        blur = cv2.medianBlur(hsv, 11)
+
+    # morphology
+    mask_p = cv2.inRange(blur, (71, 95, 0), (179, 255, 255))
+    ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    mask_close_p = cv2.morphologyEx(mask_p, cv2.MORPH_CLOSE, ker_p)
+    mask_open_p = cv2.morphologyEx(mask_close_p, cv2.MORPH_OPEN, ker_p)
+    # cv2.imshow('maks', mask_open_p)
+
+    # object counting
+    ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilatet = cv2.dilate(mask_open_p, ker_p, iterations=1)
+
+    cnt_p, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result display and human check
+    print('Counted=', len(cnt_p))
+    p_mask_final = cv2.cvtColor(mask_open_p, cv2.COLOR_GRAY2BGR)
+    p_final = cv2.addWeighted(p_mask_final, 0.5, img, 0.5, 0)
+
+    cv2.imshow('close mask', p_final)
+    cv2.waitKey(0)
+
+def call_counting():
+    purple_count()
+
+
+if __name__ == '__main__':
+    call_counting()
