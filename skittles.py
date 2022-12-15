ff6d3582b07c4de41c9f15579a12510950bcdb82
CoyotWilly <coyotwilly@gmail.com>
2022-12-15 19:24:31 +0100

    RED 2 mistakes AND PURPLE dump ideas

--- skittles.py
+++ skittles.py
@@ -114,15 +114,17 @@ def yellow_count():
     return counted
 
 def red_count():
-    blur = 0
+    blur = hsv
     for i in range(5):
-        blur = cv2.medianBlur(hsv, 11)
+        blur = cv2.medianBlur(blur, 11)
 
     # morphology
     mask_r = cv2.inRange(blur, (170, 102, 153), (179, 255, 255))
-    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
-    mask_close_r = cv2.morphologyEx(mask_r, cv2.MORPH_CLOSE, (1, 1))
-    mask_open_r = cv2.morphologyEx(mask_close_r, cv2.MORPH_OPEN, (7, 7))
+    # ker (10, 10) AND ker(3, 3)
+    # mask_r = cv2.inRange(blur, (170, 135, 100), (179, 255, 255))
+    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask_close_r = cv2.morphologyEx(mask_r, cv2.MORPH_CLOSE, ker_r)
+    mask_open_r = cv2.morphologyEx(mask_close_r, cv2.MORPH_OPEN, ker_r)
     # cv2.imshow('maks', mask_open_r)
 
     # object counting
@@ -133,71 +135,81 @@ def red_count():
 
     # final result display and human check
     counted = len(count_cnts(cnt_r))
-    print('Counted=', counted)
-    r_mask_final = cv2.cvtColor(mask_open_r, cv2.COLOR_GRAY2BGR)
-    r_final = cv2.addWeighted(r_mask_final, 0.5, img, 0.5, 0)
+    # print('Counted=', counted)
+    # r_mask_final = cv2.cvtColor(mask_open_r, cv2.COLOR_GRAY2BGR)
+    # r_final = cv2.addWeighted(r_mask_final, 0.5, img, 0.5, 0)
 
     # cv2.imshow('close mask', r_final)
-    cv2.imwrite('test.jpg', r_final)
+    # cv2.imwrite('test.jpg', r_mask_final)
     # cv2.waitKey(0)
     return counted
 
 
 def purple_count():
-    blur = 0
+    blur = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     for i in range(5):
-        blur = cv2.medianBlur(hsv, 11)
-
+        blur = cv2.medianBlur(blur, 11)
+    # mask_p = cv2.inRange(blur, (0, 127, 120), (108, 132, 140))
     # morphology
-    mask_p = cv2.inRange(blur, (71, 95, 0), (179, 255, 255))
-    ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
-    mask_close_p = cv2.morphologyEx(mask_p, cv2.MORPH_CLOSE, ker_p)
-    mask_open_p = cv2.morphologyEx(mask_close_p, cv2.MORPH_OPEN, ker_p)
+    # mask_p = cv2.inRange(blur, (71, 95, 0), (179, 255, 255))
+    mask_p = cv2.inRange(blur, (160, 85, 0), (168, 185, 255))
+    # ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    # mask_close_p = cv2.morphologyEx(mask_p, cv2.MORPH_CLOSE, (1, 1))
+    # mask_open_p = cv2.morphologyEx(mask_close_p, cv2.MORPH_OPEN, (10, 10))
     # cv2.imshow('maks', mask_open_p)
 
     # object counting
 
-    ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    dilatet = cv2.dilate(mask_open_p, ker_p, iterations=1)
+    ker_p = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    dilatet = cv2.dilate(mask_p, ker_p, iterations=1)
 
     cnt_p, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # final result display and human check
-    print('Counted=', len(cnt_p))
-    p_mask_final = cv2.cvtColor(mask_open_p, cv2.COLOR_GRAY2BGR)
+    counted = len(count_cnts(cnt_p))
+    # counted = len(cnt_p)
+    print('Counted=', counted)
+    p_mask_final = cv2.cvtColor(mask_p, cv2.COLOR_GRAY2BGR)
     p_final = cv2.addWeighted(p_mask_final, 0.5, img, 0.5, 0)
 
-    cv2.imshow('close mask', p_final)
+    cv2.imshow('final', p_final)
+    # cv2.imwrite('test.jpg', p_final)
     cv2.waitKey(0)
+    return counted
 
 
 def call_counting():
+    # GREEN 97.5%
+    # YELLOW 97.5%
+    # RED 95%
+    # PURPLE %
     green_count()
     yellow_count()
     red_count()
+    purple_count()
 
 
 if __name__ == '__main__':
-    # pic 20
-    # img = cv2.imread('data/07.jpg', cv2.IMREAD_COLOR)
-    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # red_count()
-    file = open('the_truth.json')
-    data = json.load(file)
-    val = 0
-    sum = 0
-    for i in range(39):
-        if i < 10:
-            img_path = "data/0" + str(i) + ".jpg"
-            val = "0"+ str(i) + ".jpg"
-        else:
-            img_path = "data/" + str(i) + ".jpg"
-            val = str(i) + ".jpg"
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        perfect = data[val]["yellow"]
-        return_val = yellow_count()
-        sum = sum + np.abs(perfect - return_val)
-        if return_val != perfect:
-            print('blad na zdjeciu '+ str(i))
-    print(sum)
+    # pic 20 17 32
+    img = cv2.imread('data/00.jpg', cv2.IMREAD_COLOR)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    purple_count()
+    # file = open('the_truth.json')
+    # data = json.load(file)
+    # val = 0
+    # sum = 0
+    # for i in range(39):
+    #     if i < 10:
+    #         img_path = "data/0" + str(i) + ".jpg"
+    #         val = "0"+ str(i) + ".jpg"
+    #     else:
+    #         img_path = "data/" + str(i) + ".jpg"
+    #         val = str(i) + ".jpg"
+    #     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    #     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #     perfect = data[val]["purple"]
+    #     return_val = purple_count()
+    #     sum = sum + np.abs(perfect - return_val)
+    #     if return_val != perfect:
+    #         print('blad na zdjeciu '+ str(i))
+    # print(sum)
