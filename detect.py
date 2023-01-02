010c6f39dab3d3f28c00e5fdff3b108f9a459998
CoyotWilly <coyotwilly@gmail.com>
2023-01-02 21:57:05 +0100

    detection code added

--- detect.py
+++ detect.py
@@ -8,51 +8,125 @@ from tqdm import tqdm
 import numpy as np
 
 
+def count_cnts(cnts: list, color_called: str, img: np.ndarray) -> np.ndarray:
+    area_array = np.array([0])
+    shape = np.shape(img)
+    if color_called == "purple":
+        condition = (shape[0] * shape[1]) * 0.000075
+    else:
+        condition = (shape[0] * shape[1]) * 0.00005
+
+    for cnt in cnts:
+        area = cv2.contourArea(cnt)
+        if area > condition:
+            area_array = np.append(area_array, area)
+    area_array = np.delete(area_array, 0)
+    return area_array
+
+
+def green_count(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # blurring
+    blur = cv2.medianBlur(hsv, 11)
+
+    # morphology and dilation
+    mask_g = cv2.inRange(blur, (35, 189, 0), (69, 255, 190))
+    ker = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
+    mask_close_g = cv2.morphologyEx(mask_g, cv2.MORPH_CLOSE, ker)
+    mask_open_g = cv2. morphologyEx(mask_close_g, cv2.MORPH_OPEN, ker)
+    ker_g = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    dilatet = cv2.dilate(mask_open_g, ker_g, iterations=1)
+
+    # object counting
+    cnt_g, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result calc
+    counted = len(count_cnts(cnt_g, "green", img))
+
+    return counted
+
+def yellow_count(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # blurring
+    blur = cv2.medianBlur(hsv, 11)
+
+    # morphology and dilation
+    mask_y = cv2.inRange(blur, (0, 220, 116), (32, 255, 255))
+    ker_y_open = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    ker_y_close = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    mask_close_y = cv2.morphologyEx(mask_y, cv2.MORPH_CLOSE, ker_y_close)
+    mask_open_y = cv2.morphologyEx(mask_close_y, cv2.MORPH_OPEN, ker_y_open)
+    ker_y = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    dilatet = cv2.dilate(mask_open_y, ker_y, iterations=1)
+
+    # object counting
+    cnt_y, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result calc
+    counted = len(count_cnts(cnt_y, "yellow", img))
+
+    return counted
+
+def red_count(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # blurring and threshold appliance
+    blur = cv2.medianBlur(hsv, 11)
+    mask_r = cv2.inRange(blur, (170, 102, 153), (179, 255, 255))
+
+    # morphology and dilation
+    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask_close_r = cv2.morphologyEx(mask_r, cv2.MORPH_CLOSE, ker_r)
+    mask_open_r = cv2.morphologyEx(mask_close_r, cv2.MORPH_OPEN, ker_r)
+    ker_r = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    dilatet = cv2.dilate(mask_open_r, ker_r, iterations=1)
+
+    # object counting
+    cnt_r, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result calc
+    counted = len(count_cnts(cnt_r, "red", img))
+    return counted
+
+
+def purple_count(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # blurring and threshold appliance
+    blur = cv2.medianBlur(hsv, 11)
+
+    # dilation
+    mask_range = cv2.inRange(blur, (160, 82, 0), (176, 234, 101))
+    mask_dil = cv2.dilate(mask_range, (10,10))
+
+    # object counting
+    cnt, _ = cv2.findContours(mask_dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # final result calc
+    counted = len(count_cnts(cnt, "purple", img))
+
+    return counted
+
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
-
     Parameters
     ----------
     img_path : str
         Path to processed image.
-
     Returns
     -------
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    #TODO: Implement detection method.
-    # (hMin = 23 , sMin = 36, vMin = 63), (hMax = 179 , sMax = 255, vMax = 255)
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    
-    red = 0
-    yellow = 0
-    green = 0
-    purple = 0
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lb = np.array([23, 36, 63], np.uint8)
-    ub = np.array([179, 255, 255], np.uint8)
+    red = red_count(img)
+    yellow = yellow_count(img)
+    green = green_count(img)
+    purple = purple_count(img)
 
-    mask = cv2.inRange(hsv, lb, ub)
-
-    blure = cv2.GaussianBlur(mask, (3, 3), 0)
-    eges = cv2.Canny(blure, 10, 100)
-    # eges = cv2.bilateralFilter(eges, 9, 75, 75)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    dilatet = cv2.dilate(eges, kernel, iterations=1)
-
-    # contours
-    contours, _ = cv2.findContours(dilatet, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    img2 = img.copy()
-    cv2.drawContours(img2, contours, -1, (0, 255, 0), 2)
-    print(len(contours))
-    #
-    cv2.imshow('Image', img2)
-    cv2.imshow('Orginal', img)
-    cv2.imshow('dilitate', eges)
-    cv2.waitKey()
     return {'red': red, 'yellow': yellow, 'green': green, 'purple': purple}
 
 
@@ -74,4 +148,4 @@ def main(data_path: Path, output_file_path: Path):
 
 if __name__ == '__main__':
     # main()
-    detect('data/25.jpg')
+    print(detect('data/25.jpg'))
